2ac160bd7e7828cc0821f768d219d5b1cc86ef55
Sam Escolas <samescolas@gmail.com>
2018-04-13 18:42:27 -0400

    Cleaning up code a bit.

--- chat.c
+++ chat.c
@@ -7,30 +7,60 @@
 #include <sys/types.h>
 
 #define BUF_LEN 1024
-#define MAX_NAME_LENGTH 18
 #define DEVA "/dev/hey"
 #define DEVB "/dev/hi"
 #define DEVICE_NAME_LENGTH 8
 
-int		assign_name(int argc, char **argv, char *name)
+int		assign_names(int argc, char **argv, char *me, char *you)
 {
 	if (argc == 2)
 	{
-		strncpy(name, &argv[1][0], MAX_NAME_LENGTH);
+		*me = argv[1][0];
+		*you = (*me == 'a' ? 'b' : 'a');
+		if (*me != 'a' && me[0] != 'b')
+			return (-1);
 		return (0);
 	}
 	return (-1);
 	
 }
 
-int		main(int argc, char **argv)
+void	write_user_prompt(char username)
 {
-	char	name[MAX_NAME_LENGTH+1];
+	write(1, "[", 1);
+	write(1, &username, 1);
+	write(1, "] ", 2);
+}
+
+void	read_inbox(int fd, char me, char you)
+{
+
+	int 	bytes_read;
 	char	buf[BUF_LEN+1];
 
-	bzero(name, MAX_NAME_LENGTH+1);
-	bzero(buf, BUF_LEN);
-	if (assign_name(argc, argv, name) < 0)
+	bzero(buf, BUF_LEN + 1);
+	while (1)
+	{
+		// Do we want to rate limit these read requests or is this okay?
+		// for (int tl=0; tl<1000000; tl++);
+		if ((bytes_read = read(fd, &buf, BUF_LEN)) > 0)
+		{
+			// clear current line
+			write(1, "\r", 1);
+			write_user_prompt(you);
+			write(1, buf, strlen(buf));
+			write_user_prompt(me);
+			bzero(buf, bytes_read);
+		}
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	char	me;
+	char	you;
+
+	if (assign_names(argc, argv, &me, &you) < 0)
 	{
 		printf("usage:\t./chat a|b\n");
 		return (0);
@@ -43,8 +73,8 @@ int		main(int argc, char **argv)
 	bzero(read_device, DEVICE_NAME_LENGTH);
 	bzero(write_device, DEVICE_NAME_LENGTH);
 	/* Users will open opposite files for reading and writing. */
-	strncpy(read_device, strcmp(name, "a") ? DEVA : DEVB, DEVICE_NAME_LENGTH);
-	strncpy(write_device, strcmp(name, "a") ? DEVB : DEVA, DEVICE_NAME_LENGTH);
+	strncpy(read_device, me == 'a' ? DEVA : DEVB, DEVICE_NAME_LENGTH);
+	strncpy(write_device, me == 'a' ? DEVB : DEVA, DEVICE_NAME_LENGTH);
 	read_device[DEVICE_NAME_LENGTH] = '\0';
 	write_device[DEVICE_NAME_LENGTH] = '\0';
 	if ((fds[0] = open(read_device, O_RDONLY)) < 0)
@@ -67,53 +97,32 @@ int		main(int argc, char **argv)
 		printf("Opened %s for writing.\n", write_device);
 	}
 
-	pid_t	child;
+	pid_t	child = fork();
 
-	child = fork();
 	if (child >= 0)
 	{
 		if (child == 0)
 		{
-			int bytes_read;
-			// child will handle reading
-			while (1)
-			{
-				for(int tl=0; tl<1000000; tl++);
-				if ((bytes_read = read(fds[0], &buf, BUF_LEN)) > 0)
-				{
-					write(1, "\r", 1);
-					buf[bytes_read] = '\0';
-					printf("[%s]\t%s", strcmp(name, "a") ? "a" : "b", buf);
-					write(1, "[", 1);
-					write(1, name, 1);
-					write(1, "]\t", 2);
-				}
-			}
+			read_inbox(fds[0], me, you);
 		}
 		else
 		{
-			// parent will handle gathering input
 			char	read_buf[BUF_LEN+1];
 			int		bytes;
 
+			bzero(read_buf, BUF_LEN + 1);
 			while (1)
 			{
-				bzero(read_buf, BUF_LEN);
-				write(1, "[", 1);
-				write(1, name, 1);
-				write(1, "]\t", 2);
+				write_user_prompt(me);
 				if ( (bytes = read(0, &read_buf, BUF_LEN)) > 0)
 				{
 					write(fds[1], read_buf, bytes);
+					bzero(read_buf, bytes);
 				}
 			}
 		}
 	}
 	close(fds[0]);
 	close(fds[1]);
-	//read(fd, &buf, BUF_LEN);
-	//buf[BUF_LEN] = '\0';
-	//printf("Message received from %s: %s\n", DEV1, buf);
-	//write(fd, "Hey there.\n", 11);
 	return (0);
 }
